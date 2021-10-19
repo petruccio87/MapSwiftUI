12d2c36adcec4f49d99f4c31bf90d859483bc352
crazyreleaser <crazyreleaser@yandex.ru>
2021-10-19 11:09:48 +0300

    Initial Commit

---
+++ MapSwiftUI/MapSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapSwiftUIApp.swift
+//  MapSwiftUI
+//
+//  Created by admin on 19.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MapSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
