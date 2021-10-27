3a9489b0b2a5ea26c5df4738e77376faf80bc159
crazyreleaser <crazyreleaser@yandex.ru>
2021-10-27 13:21:25 +0300

    add models and search function

--- MapSwiftUI/ContentView.swift
+++ MapSwiftUI/ContentView.swift
@@ -11,14 +11,37 @@ import MapKit
 struct ContentView: View {
     
     @ObservedObject private var locationManager = LocationManager()
+    @State private var search: String  = ""
+    @State private var landmarks = [Landmark]()
+    
+    private func getNearByLandmarks() {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = search
+        
+        let search = MKLocalSearch(request: request)
+        search.start { (responce, error) in
+            if let responce = responce {
+                let mapItems = responce.mapItems
+                landmarks = mapItems.map {
+                    Landmark(placemark: $0.placemark)
+                }
+            }
+        }
+    }
     
     var body: some View {
         
         let coordinate = self.locationManager.location != nil ?
             self.locationManager.location!.coordinate : CLLocationCoordinate2D()
         
-        ZStack{
+        ZStack(alignment: .top) {
             MapView()
+            TextField("Search", text: $search, onEditingChanged: { _ in })
+            {
+                //commit
+            }.textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .offset(y: 44)
 //            Text("\(coordinate.latitude), \(coordinate.longitude)")
 //                .foregroundColor(.white)
 //                .padding()
--- MapSwiftUI/Managers/LocationManager.swift
+++ MapSwiftUI/Managers/LocationManager.swift
@@ -26,6 +26,10 @@ class LocationManager: NSObject, ObservableObject {
 
 extension LocationManager: CLLocationManagerDelegate {
     
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print(status)
+    }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else {return}
         self.location = location
